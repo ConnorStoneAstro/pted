ca01298488461b8c1d33642130751080f0407917
Connor Stone <connorstone628@gmail.com>
2025-04-25 14:18:22 -0400

    simpler core pted calculation

--- src/pted/utils.py
+++ src/pted/utils.py
@@ -5,12 +5,10 @@ import torch
 __all__ = ["_pted_numpy", "_pted_torch"]
 
 
-def _energy_distance_precompute(D, ix, iy):
-    nx = len(ix)
-    ny = len(iy)
-    Exx = (D[ix.reshape(nx, 1), ix.reshape(1, nx)]).sum() / nx**2
-    Eyy = (D[iy.reshape(ny, 1), iy.reshape(1, ny)]).sum() / ny**2
-    Exy = (D[ix.reshape(nx, 1), iy.reshape(1, ny)]).sum() / (nx * ny)
+def _energy_distance_precompute(D, nx, ny):
+    Exx = D[:nx, :nx].sum() / nx**2
+    Eyy = D[nx:, nx:].sum() / ny**2
+    Exy = D[:nx, nx:].sum() / (nx * ny)
     return 2 * Exy - Exx - Eyy
 
 
@@ -22,13 +20,14 @@ def _pted_numpy(x, y, permutations=100, metric="euclidean", return_all=False):
         np.isfinite(dmatrix)
     ), "Distance matrix contains NaN or Inf! Consider using a different metric or normalizing values to be more stable (i.e. z-score norm)."
     nx = len(x)
-    I = np.arange(len(z))
+    ny = len(y)
 
-    test_stat = _energy_distance_precompute(dmatrix, I[:nx], I[nx:])
+    test_stat = _energy_distance_precompute(dmatrix, nx, ny)
     permute_stats = []
     for _ in range(permutations):
-        np.random.shuffle(I)
-        permute_stats.append(_energy_distance_precompute(dmatrix, I[:nx], I[nx:]))
+        I = np.random.permutation(len(z))
+        dmatrix = dmatrix[I][:, I]
+        permute_stats.append(_energy_distance_precompute(dmatrix, nx, ny))
     if return_all:
         return test_stat, permute_stats
     # Compute p-value
@@ -46,13 +45,14 @@ def _pted_torch(x, y, permutations=100, metric="euclidean", return_all=False):
         torch.isfinite(dmatrix)
     ), "Distance matrix contains NaN or Inf! Consider using a different metric or normalizing values to be more stable (i.e. z-score norm)."
     nx = len(x)
-    I = torch.arange(len(z))
+    ny = len(y)
 
-    test_stat = _energy_distance_precompute(dmatrix, I[:nx], I[nx:]).item()
+    test_stat = _energy_distance_precompute(dmatrix, nx, ny).item()
     permute_stats = []
     for _ in range(permutations):
-        I = I[torch.randperm(len(I))]
-        permute_stats.append(_energy_distance_precompute(dmatrix, I[:nx], I[nx:]).item())
+        I = torch.randperm(len(z))
+        dmatrix = dmatrix[I][:, I]
+        permute_stats.append(_energy_distance_precompute(dmatrix, nx, ny).item())
     if return_all:
         return test_stat, permute_stats
     # Compute p-value
