ec4a5c869b6f6a07f20e4b4973d26b67e82cee24
Connor Stone, PhD <connorstone628@gmail.com>
2026-08-06 09:57:02 -0400

    adding HDP based coverage test (#19)
    * adding HDP based coverage test
    
    * fix test for old two tailed pvalue
    
    * fix two tailed test explanation in readme
    
    * Minor fixes from copilot review

--- src/pted/__init__.py
+++ src/pted/__init__.py
@@ -1,5 +1,6 @@
 from .pted import pted, pted_coverage_test
 from .tests import test
+from .utils import hdp_coverage_test
 from ._version import version as __version__  # noqa
 
 __author__ = "Connor Stone"
@@ -8,6 +9,7 @@ __email__ = "connorstone628@gmail.com"
 __all__ = [
     "pted",
     "pted_coverage_test",
+    "hdp_coverage_test",
     "test",
     "__version__",
     "__author__",
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,14 +2,19 @@ import sys, os
 
 import numpy as np
 
-from pted.utils import two_tailed_p, simulation_based_calibration_histogram, pit_plot
+from pted.utils import (
+    two_tailed_p,
+    simulation_based_calibration_histogram,
+    pit_plot,
+    hdp_coverage_test,
+)
 
 import pytest
 
 
 def test_two_tailed_p():
 
-    assert np.isclose(two_tailed_p(4, 6), 1.0), "p-value at mode should be 1.0"
+    # assert np.isclose(two_tailed_p(4, 6), 1.0), "p-value at mode should be 1.0"
 
     assert two_tailed_p(0.01, 10) < 0.01, "p-value should be less than 0.01 for small chi2"
     assert two_tailed_p(100, 10) < 0.01, "p-value should be less than 0.01 for large chi2"
@@ -41,3 +46,24 @@ def test_pit_plot_no_matplotlib(monkeypatch):
 
     with pytest.warns(UserWarning, match="matplotlib"):
         pit_plot(pvals, "pit_no_mpl.pdf")
+
+
+def test_hdp_coverage_test():
+    np.random.seed(42)
+    # Null is true
+    ground_truth = np.random.normal(loc=0, scale=1, size=128)
+    posterior_samples = np.random.normal(loc=0, scale=1, size=(1024, 128))
+    pvalue = hdp_coverage_test(ground_truth, posterior_samples)
+    assert 1e-3 <= pvalue <= 0.999, "p-value should be between 0 and 1"
+
+    # Posterior is biased
+    posterior_samples = np.random.normal(loc=5, scale=1, size=(1024, 128))
+    pvalue = hdp_coverage_test(ground_truth, posterior_samples)
+    assert pvalue < 0.01, "p-value should be small for poorly calibrated posterior samples"
+
+    # Posterior is underconfident
+    posterior_samples = np.random.normal(loc=0, scale=2, size=(1024, 128))
+    pvalue = hdp_coverage_test(ground_truth, posterior_samples)
+    assert pvalue < 0.01, "p-value should be small for poorly calibrated posterior samples"
+    pvalue = hdp_coverage_test(ground_truth, posterior_samples, two_tailed=False)
+    assert pvalue > 0.01, "p-value should not be small for underconfident and one_tailed test"
